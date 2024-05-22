ab16338b45bc53b563479244dedf2baec549e583
Jose Fernández <josegfc95@gmail.com>
2024-05-21 22:35:16 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gregfc95.github.io/',
-  base: '/gregfc95.github.io',
   prefetch: true,
   integrations: [tailwind()],
 });
